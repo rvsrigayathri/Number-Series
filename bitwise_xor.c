e3be9c0e4415c29782c88dfc2526cf2fa00d188a
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-06-20 06:01:42 +0530

    Add files via upload

---
+++ bitwise_xor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+   int num1,num2,result;
+   scanf("%d %d",&num1,&num2);
+   result = num1^num2;
+   printf("%d",result);
+   return 0;
+}
+
+/*
+    #include <stdio.h>
+    int main()
+    {
+        int num1,num2;
+        scanf("%d %d",&num1,&num2);
+        printf("%d",num1^num2);
+        return 0;
+    }
+*/
