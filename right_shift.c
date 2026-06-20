079a8461e4fc5e7ea1e98755ecf20471116b4ddc
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-06-20 06:08:19 +0530

    Add files via upload

---
+++ right_shift.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+   int num1,n,result;
+   scanf("%d %d",&num1,&n);
+   result = num1>>n;
+   printf("%d",result);
+   return 0;
+}
+
+/*
+    #include <stdio.h>
+    int main()
+    {
+        int num1,n;
+        scanf("%d %d",&num1,&n);
+        printf("%d",num1>>n);
+        return 0;
+    }
+*/
