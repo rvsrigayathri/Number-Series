87a5039579a7df3276ec0fe6a7e6fb797723ef19
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-06-22 06:35:29 +0530

    Add files via upload

---
+++ palindrome_triangle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+    int num,i,j;
+
+    scanf("%d",&num);
+
+    for(int i=1;i<=num;i++) {
+
+        for(int j=1;j<=i;j++) {
+            printf("%d ",j);
+        }
+        
+        for(int j=i-1;j>=1;j--) {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
