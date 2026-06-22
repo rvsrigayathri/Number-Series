aa0fd7c076a02cf217f890a23391fde837d4bd46
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-06-22 06:44:14 +0530

    Add files via upload

---
+++ perfect_check.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int num,i,sum;
+    scanf("%d",&num);
+    for(int i=1;i<=num/2;i++){
+        if(num%i==0){
+            sum=sum+i;
+        }
+    }
+    if(sum==num){
+        printf("Perfect number");
+    }
+    else{
+        printf("Not a Perfect number");
+    }
+    return 0;
+}
