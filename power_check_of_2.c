80e7b071835f37bb67fd402eb691e4a10203e2f1
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-06-22 06:51:46 +0530

    Add files via upload

---
+++ power_check_of_2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+    if(num>0 && (2^num==2) && num!=10 && num%3!=0){
+        printf("Perfect number of power 2");
+    }
+    else{
+        printf("Not a perfect of power 2");
+    }
+    return 0;
+}
+
+
+/*
+#include<stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+    if(num>0 && (num&(num-1))==0){
+        printf("%d is power by 2",num);    
+    }
+    else{
+        printf("%d is not power of 2",num);
+    }
+    return 0;
+}
+*/
